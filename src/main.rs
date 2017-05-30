4d7a803e9dd6dbebb29dfb93c73bd47130a2d1a6
Johan Fjeldtvedt <jaffe1@gmail.com>
2017-05-30 02:00:28 +0200

    panics are now working a bit

--- src/main.rs
+++ src/main.rs
@@ -13,48 +13,52 @@ fn rust_begin_panic(_msg: core::fmt::Arguments, _file: &'static str, _line: u32)
 pub extern fn rust_eh_personality() {
 }
 
-macro_rules! breakpoint {
-    ($arg:expr) => (
-        unsafe { asm!("BKPT $0" : : "i"($arg) : : "volatile") }
-    )
-}
-
-extern {
-    static __StackTop: u32;
-}
-
-fn pin_output() {
+fn pin_output(pin: u32) {
     const P_DIR: u8 = 0;
     const P_INPUT: u8 = 1;
     const P_PULL: u8 = 2;
     const P_DRIVE: u8 = 8;
     const P_SENSE: u8 = 16;
 
-    let cnf = 0x50000744 as *mut u32;
-
     unsafe {
+        let cnf = (0x50000700 + 4 * pin) as *mut u32;
         *cnf = (1 << P_DIR) | (1 << P_INPUT) | (0 << P_PULL) | (0 << P_DRIVE) | (0 << P_SENSE);
     }
 }
 
-fn pin_outset() {
+fn pin_set(pin: u32, state: bool) {
+    let outclear = 0x5000050C as *mut u32;
     let outset = 0x50000508 as *mut u32;
 
     unsafe {
-        *outset = 1 << 17;
+        if state {
+            *outclear = 1 << pin;
+        } else {
+            *outset = 1 << pin;
+        }
+    }
+}
+
+fn burn() {
+    let mut x: i32 = 0;
+    for i in 0..100000 {
+        x += i;
+        x -= i;
     }
 }
 
 #[naked]
 pub fn main() {
-    // Set up stack
-    unsafe {
-        asm!("mov sp, $0"::"r"(__StackTop)::"volatile");
-    }
+    let mut led_state = false;
 
-    pin_output();
-    pin_outset();
-    breakpoint!(1);
+    loop {
+        pin_output(17);
+        pin_output(19);
+        pin_set(17, led_state);
+        pin_set(19, led_state);
+        burn();
+        led_state = led_state ^ true;
+    }
 }
 
 #[link_section = ".reset_handler"]
